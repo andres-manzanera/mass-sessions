1e312b15f977f1efeb2e46c9da59bf251f3c0d29
andres-manzanera <105920485+andres-manzanera@users.noreply.github.com>
2026-06-26 18:25:54 +0200

    Ajuste textos hero en ipad landscape y portrai (2)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,7 +72,7 @@ export default function Home() {
       <main className="pt-20 min-h-screen flex flex-col">
         
         {/* Hero Section - Massive Background Image & Text */}
-        <section className="relative w-full min-h-[calc(100dvh-5rem)] md:h-[calc(100vh-5rem)] md:min-h-[700px] border-b-2 border-brand-orange flex flex-col justify-end p-6 pb-24 md:p-16 md:pb-16 lg:pb-16 overflow-hidden hero-section">
+        <section className="relative w-full min-h-[calc(100dvh-5rem)] md:min-h-[calc(100vh-5rem)] md:min-h-[700px] border-b-2 border-brand-orange flex flex-col justify-end p-6 pb-24 md:p-16 md:pb-16 lg:pb-16 hero-section">
           {/* Zooming background image wrapper */}
           <div className="absolute inset-0 overflow-hidden z-0">
             <Image
